56370867af61d3d8c35a6fc96f1345e7e45ef8ed
Hoang381202 <phuonglien3802@gmail.com>
2024-12-13 03:05:43 +0700

    Add files via upload

---
+++ cutchanv3,.lua
@@ -0,0 +1,15 @@
+repeat task.wait() until game:IsLoaded()
+local TablePlace = {7449423635,2753915549,4442272183}
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "nhìn", -- Required
+	Text = "success", -- Required
+	Icon = "121838630736220", -- Optional
+	Duration = 5
+})
+if table.find(TablePlace,game.PlaceId) then
+    _G.Game = "BF"
+    _G.Key = "Xzt7M9IAfF"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/BF-New.lua"))()
+else
+    game.Players.LocalPlayer:Kick("địt mẹ ngu game này dell hỗ trợ")
+end
